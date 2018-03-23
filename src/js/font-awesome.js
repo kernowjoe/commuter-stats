497a5b804644a5e235e82a06109f9cacbfb77da6
Joe Prisk <joe.prisk@headforwards.com>
2018-03-23 18:00:36 +0000

    font awesome tweak

--- src/js/font-awesome.js
+++ src/js/font-awesome.js
@@ -2,23 +2,25 @@ import fontawesome from '@fortawesome/fontawesome'
 
 import faBicycle from '@fortawesome/fontawesome-free-solid/faBicycle'
 import faCircle from '@fortawesome/fontawesome-free-solid/faCircle'
-import fatree from '@fortawesome/fontawesome-free-solid/fatree'
-import fautensils from '@fortawesome/fontawesome-free-solid/faUtensils'
-import famoney from '@fortawesome/fontawesome-free-solid/faMoneyBillAlt'
-import faspinner from '@fortawesome/fontawesome-free-solid/faSpinner'
-import facircleonotch from '@fortawesome/fontawesome-free-solid/faCircleNotch'
+import faTree from '@fortawesome/fontawesome-free-solid/fatree'
+import faUtensils from '@fortawesome/fontawesome-free-solid/faUtensils'
+import faMoney from '@fortawesome/fontawesome-free-solid/faMoneyBillAlt'
+import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner'
+import faCircleonotch from '@fortawesome/fontawesome-free-solid/faCircleNotch'
 import faCog from '@fortawesome/fontawesome-free-solid/faCog';
 
-export {load };
+export {load};
 
 function load() {
 
-    fontawesome.library.add(faBicycle);
-    fontawesome.library.add(faCircle);
-    fontawesome.library.add(fatree);
-    fontawesome.library.add(fautensils);
-    fontawesome.library.add(famoney);
-    fontawesome.library.add(faspinner);
-    fontawesome.library.add(facircleonotch);
-    fontawesome.library.add(faCog);
+    fontawesome.library.add(
+        faBicycle,
+        faCircle,
+        faTree,
+        faUtensils,
+        faMoney,
+        faSpinner,
+        faCircleonotch,
+        faCog
+    );
 }
